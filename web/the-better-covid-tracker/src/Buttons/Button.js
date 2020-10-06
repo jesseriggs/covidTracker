c03e2b995dde597e059c2be6cb0d5285e387f1a6
Jesse Riggs <jesseriggs@u.boisestate.edu>
2020-10-06 12:20:20 -0600

    Adding Buttons
    This was supposed to be part of an earlier commit...

---
+++ web/the-better-covid-tracker/src/Buttons/Button.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { Button } from 'arwes';
+
+class Buttons extends Component
+{
+	render(){
+		return(
+			<Button type={"submit"}>Submit</Button>
+		);
+	}
+}
+
+export { Buttons };
