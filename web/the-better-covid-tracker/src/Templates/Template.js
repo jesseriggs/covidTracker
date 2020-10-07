2e08e1763a7872b86df80c0c8ce4ff1e112ed3ba
Jesse Riggs <jesseriggs@u.boisestate.edu>
2020-10-07 07:49:35 -0600

    Added footer to template and some nit-picking.
    Spent some time on getting the page to look "correct".

--- web/the-better-covid-tracker/src/Templates/Template.js
+++ web/the-better-covid-tracker/src/Templates/Template.js
@@ -5,6 +5,15 @@ import '../styles/index.css'
 
 const lorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
 
+const elem = (document.compatMode === "CSS1Compat") ? 
+	document.documentElement :
+	document.body;
+
+const height = elem.clientHeight;
+const width = elem.clientWidth;
+
+const maxH = ( height - 132 ) + "px";
+
 class MainContainer extends Component
 {
 	render(){
@@ -14,10 +23,13 @@ class MainContainer extends Component
 			<div className={"main-container"}>
 			  <Frame
 				show={true}
-				style={{height:"85%"}} >
+				style={{height:"100%"}} >
 				<div style={{
 					backgroundColor:"#02111488",
-					padding:"20px"}}>
+					padding:"20px",
+					maxHeight:maxH,
+					overflowY:"scroll",
+					scrollbarWidth:"none"}}>
 				  {typeof children === 'function'?
 					  children():
 					  children}
@@ -37,11 +49,36 @@ class DescriptionPane extends Component
 			<div className={"description-pane"}>
 			<Frame
 				style={{height:"100%"}}>
-					<div style={{height:"75vh",padding:"20px"}}>
-						<h1>Text Pane</h1>
-						<p>{text}</p>
-					</div>
+				<div style={{
+					padding:"20px",
+					maxHeight:maxH,
+					overflowY:"scroll",
+					scrollbarWidth:"none"}}
+					>
+					<h1>Text Pane</h1>
+					<p>{text}</p>
+				</div>
 			</Frame>
+			</div>
+		);
+	}
+}
+
+class Footer extends Component
+{
+	render(){
+		const children = this.props.children;
+		return(
+			<div style={{
+				height:"50px",
+				position:"relative",
+				backgroundColor:"#02111488",
+				marginTop:"10px",
+				fontSize:"12px"}}
+				>
+				<Frame style={{height:"50px",padding:"5px"}}>
+					{children}
+				</Frame>
 			</div>
 		);
 	}
@@ -49,24 +86,25 @@ class DescriptionPane extends Component
 
 class Template extends Component
 {
-
 	render(){
 		return(
 			<ThemeProvider theme={createTheme()}>
 			  <Arwes classes={{pattern:"corona-pattern"}} animate show>
 			    <div>
-				<p>This is a template.</p>
 				<ToolBar>
-				    <span>
+				    <span style={{marginLeft:"60px"}}>
 				        This is where tools go
 				    </span>
 				</ToolBar>
-				<div style={{display:"flex",paddingLeft:"5vw",paddingRight:"5vw"}}>
+				<div style={{display:"flex",paddingLeft:"5vw",paddingRight:"5vw",marginTop:"60px"}}>
 					<MainContainer>
 						{this.props.children}
 					</MainContainer>
 					<DescriptionPane />
 				</div>
+				<Footer>
+					<span>This is a footer</span>
+				</Footer>
 			    </div>
 			  </Arwes>
 			</ThemeProvider>
